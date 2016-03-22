74f34cd5a0fb24caf40f7ba784656c7ff7bd58bc
charliebr0wn <cdelaorden@gmail.com>
2016-03-22 21:48:55 +0100

    Finish basic system

--- src/engine/index.js
+++ src/engine/index.js
@@ -1,6 +1,76 @@
-function createEngine(systems = []){
+import { Map, List, Record, fromJS } from 'immutable'
 
-  let _systems = systems.map(systemConfig => {
+const Entity = Record({
+  id: 0,
+  components: Map()
+})
 
-  })
+function verifySystemDefinition(ss){
+  return ss.reduce((acc, s) => {
+    if(typeof s.name !== 'string'){
+      console.error('Name is required')
+      return false
+    }
+    if(typeof s.update !== 'function'){
+      console.error('update() should be a function')
+      return false
+    }
+    if(!Array.isArray(s.comps)){
+      s.comps = []
+    }
+    return acc
+  }, true)
+}
+
+export function createEngine(systems = []){
+  let _nextId = 1,
+      _systems = systems,
+      _entities = Map(),
+      _isRunning = false,
+      _rAF = 0
+
+      if(!verifySystemDefinition(_systems)){
+        throw new Error('Invalid system definition')
+      }
+
+  function update(time){
+    _systems.forEach(s => {
+      _entities = s.update(_entities, time)
+    })
+  }
+
+  function createEntity(components = Map()){
+    let e = new Entity({ id: _nextId++, components})
+    _entities = _entities.set(e.id, e)
+    return e
+  }
+
+  function getEntities(withComps = []){
+    return withComps.length > 0 ?
+      _entities.filter(e => withComps.every(c => e.components.has(c))) :
+      _entities
+  }
+
+  function start(){
+    _isRunning = true
+    _rAF = requestAnimationFrame(update)
+  }
+
+  function stop(){
+    _isRunning = false
+    cancelAnimationFrame(_rAF)
+  }
+
+  function pause(){
+    stop()
+  }
+
+  return {
+    start,
+    stop,
+    pause,
+    createEntity,
+    getEntities,
+    isRunning: () => _isRunning
+  }
 }
--- src/test/engine/index.js
+++ src/test/engine/index.js
@@ -1,12 +1,109 @@
-import Engine from '../engine'
+import jsdom from 'mocha-jsdom'
+import raf from 'raf'
+import { createEngine } from '../../engine'
+import { Map, fromJS } from 'immutable'
+import assert from 'assert'
 
 describe('Engine', () => {
+  jsdom()
+  raf.polyfill()
 
-
-  it('Should return a game engine', () => {
-
+  it('createEngine returns a a game engine with start, pause, stop, isRunning, createEntity, getEntities methods', () => {
+    let engine = createEngine([])
+    assert.equal('function', typeof engine.start)
+    assert.equal('function', typeof engine.pause)
+    assert.equal('function', typeof engine.stop)
+    assert.equal('function', typeof engine.isRunning)
+    assert.equal('function', typeof engine.createEntity)
+    assert.equal('function', typeof engine.getEntities)
   })
 
+  it('createEntity returns a new Entity record with id and empty component map', () => {
+    let engine = createEngine(),
+        entity1 = engine.createEntity(),
+        entity2 = engine.createEntity()
 
+    assert.equal(1, entity1.id)
+    assert.equal(0, entity1.components.count())
+    assert.equal(2, entity2.id)
+    assert.equal(0, entity2.components.count())
+  })
+
+  it('createEntity accepts optional components', () => {
+    let comps = fromJS({
+      comp1: { foo: 1 },
+      comp2: { bar: 2 }
+    })
+
+    let engine = createEngine(),
+        entity = engine.createEntity(comps)
+
+    assert.equal(1, entity.id)
+    assert.equal(2, entity.components.count())
+    assert.equal(1, entity.components.get('comp1').get('foo'))
+  })
+
+  it('getEntities returns existing entities', () => {
+    const engine = createEngine(),
+          comps = fromJS({
+            size: { width: 100 }
+          }),
+          entity = engine.createEntity(comps)
+
+    const ents = engine.getEntities()
+    assert.equal(1, ents.count())
+    assert.equal(1, ents.first().id)
+    assert.equal(100, ents.first().components.getIn(['size', 'width']))
+  })
+
+  it('getEntities accepts an optional list of must-have components to filter', () => {
+    const engine = createEngine(),
+          comps = fromJS({
+            size: { width: 100 }
+          }),
+          entity = engine.createEntity(comps)
+
+    let ents = engine.getEntities(['size'])
+    assert.equal(1, ents.count())
+    let emptyEnts = engine.getEntities(['fake'])
+    assert.equal(0, emptyEnts.count())
+  })
+
+  it('start should begin rAF', () => {
+    const engine = createEngine()
+    engine.start()
+    assert.equal(engine.isRunning(), true)
+    engine.stop()
+  })
+
+  it('stop() should stop rAF', () => {
+    const engine = createEngine()
+    engine.start()
+    engine.stop()
+    assert.equal(engine.isRunning(), false)
+  })
+
+  it('during loop, systems should be called with list of entities matching its comps and its results stored', (done) => {
+    const systems = [{
+      name: 'gravity',
+      update: function(entities, dt){
+        assert.equal(1, entities.count())
+        assert.equal('number', typeof dt)
+        return entities.map(e => {
+          return e.setIn(['components', 'foo', 'bar'], 2)
+        })
+      }
+    }]
+    const engine = createEngine(systems),
+          ent = engine.createEntity(fromJS({ foo: { bar: 1 }}))
+
+    engine.start()
+    setTimeout(() => {
+      let ents = engine.getEntities()
+      assert.equal(2, ents.first().components.getIn(['foo', 'bar']))
+      done()
+      engine.stop()
+    }, 16)
+  })
 })
 
